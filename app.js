d9918c233c0168e97e5cdcafa9dda45743478e57
Zachary Terrell <zachterrell@gmail.com>
2022-06-07 15:30:34 -0500

    added tailwind bc I'm lazy

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = express()
 const port = 3000
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Instacaster listening on port ${port}`)
 })
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-	res.render('index')
+	res.render('pages/index')
 })
 
 app.get('/', (req, res) => {
