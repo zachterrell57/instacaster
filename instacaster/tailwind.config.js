37e275aa3bf3b6f1efe41e459df2733ec9543e86
zachterrell57 <zachterrell@gmail.com>
2022-07-10 13:40:14 -0500

    basic rewrite done

---
+++ instacaster/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        "lg-card": "600px",
+      },
+      fontSize: {
+        tiny: ".625rem",
+      },
+      fontFamily: {
+        "source-code-pro": ["Source Code Pro", "monospace"],
+      },
+      colors: {
+        primary: "#f5f5f5",
+        "border-gray": "#dbdbdb",
+        "dark-gray-text": "#262626",
+        "purple-main": "#8765CC",
+      },
+      minWidth: {
+        "lg-card": "600px",
+      },
+    },
+  },
+  plugins: [],
+};
